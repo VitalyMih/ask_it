eca31a07a779fad0a9de2c268103c5ea96d47b46
VitalyMih <vitalymihale@yandex.ru>
2023-12-03 12:04:36 +0300

    Добавляем возможность создания вопроса

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,4 +2,23 @@ class QuestionsController < ApplicationController
   def index
     @questions = Question.all
   end
+
+  def new
+    @question = Question.new
+  end
+
+  def create
+    @question = Question.new(question_params)
+    if @question.save
+      redirect_to questions_path
+    else
+      redirect_to new_question_path
+    end
+  end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:title, :body)
+  end
 end
