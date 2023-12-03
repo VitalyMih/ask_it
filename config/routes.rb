f54da0aa2dafb6e90a23780fed711e50de3a1be0
VitalyMih <vitalymihale@yandex.ru>
2023-12-03 12:46:37 +0300

    Добавляем возможность удаления вопроса

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root 'pages#index'
 
-  resources :questions, only: %i[index new create edit update]
+  resources :questions, only: %i[index new create edit update destroy]
 end
